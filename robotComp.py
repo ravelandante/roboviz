df16e66e7f827fa320b40da4362f39fc9caa2365
Fynn Young <yngfyn001@cs.uct.ac.za>
2022-08-16 21:12:38 +0200

    Delete robotComp.py

--- robotComp.py
+++
@@ -1,6 +0,0 @@
-class RobotComp:
-    def __init__(self, id, type, root, orientation):
-        self.id = id
-        self.type = type
-        self.root = root
-        self.orientation = orientation
