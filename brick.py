70aca5971786cfc2c45a49fe74796687586cd4eb
Daanyaal Gamieldien <GMLMOG016@myuct.ac.za>
2022-08-16 19:21:54 +0200

    Added brick and hinge child classes to the robotComp parent class. Changed robotComp objects in robotHandler to test inheritance

---
+++ brick.py
@@ -0,0 +1,5 @@
+from robotComp import RobotComp
+
+class Brick(RobotComp):
+    def __init__(self, id, type, root, orientation):
+        super().__init__(id, type, root, orientation)    
--- robotHandler.py
+++ robotHandler.py
@@ -2,14 +2,16 @@ from environment import Environment
 from robotComp import RobotComp
 from connection import Connection
 from robot import Robot
+from hinge import Hinge
+from brick import Brick
 
 app = Environment(1000, 1000, 1)
 
-comp1 = RobotComp('comp1', 'CoreComponent', True, 0)
-comp2 = RobotComp('comp2', 'FixedBrick', False, 0)
-comp3 = RobotComp('comp3', 'FixedBrick', False, 0)
-comp4 = RobotComp('comp4', 'ActiveHinge', False, 0)
-comp5 = RobotComp('comp5', 'PassiveHinge', False, 0)
+comp1 = Brick('comp1', 'CoreComponent', True, 0)
+comp2 = Brick('comp2', 'FixedBrick', False, 0)
+comp3 = Brick('comp3', 'FixedBrick', False, 0)
+comp4 = Hinge('comp4', 'ActiveHinge', False, 0)
+comp5 = Hinge('comp5', 'PassiveHinge', False, 0)
 
 connections = []
 connections.append(Connection(comp1, comp2, 0, 0))
