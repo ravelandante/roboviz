691fb688a5c82b0c718ebf0e42840303f026c9bd
fynn <fynn@fynnyoung.com>
2022-08-24 10:03:37 +0200

    add toString

--- robotComp.py
+++ robotComp.py
@@ -4,10 +4,16 @@
 # ---------------------------------------------------------------------------
 """Represents a component of a robot"""
 
+from panda3d.core import LVector3f
+
 
 class RobotComp:
-    def __init__(self, id, type, root, orientation=0):
+    def __init__(self, id, type, root, orientation=0, direction=0):
         self.id = id
         self.type = type                # component type
         self.root = root                # component is the root of the robot component tree
         self.orientation = orientation  # global orientation
+        self.direction = direction
+
+    def __str__(self):
+        return f"ID: {self.id}, Type: {self.type}, root: {self.root}, orient: {self.orientation}"
